53e2568ff1538ee5eb698890f08139710aabef9a
chajr <chajr@bluetree.pl>
2018-02-25 16:44:20 +0100

    Added customer field to store FreshSales ID

---
+++ app/code/local/Chajr/FreshSales/sql/chajr_freshsales_setup/install-0.3.0.php
@@ -0,0 +1,38 @@
+<?php
+
+/** @var $installer Mage_Catalog_Model_Resource_Eav_Mysql4_Setup */
+$installer = $this;
+$installer->startSetup();
+
+$entityTypeId = (int)$installer->getEntityTypeId('customer');
+$attributeSetId = (int)$installer->getDefaultAttributeSetId($entityTypeId);
+$attributeGroupId = (int)$installer->getDefaultAttributeGroupId($entityTypeId, $attributeSetId);
+
+$installer->addAttribute(
+    $entityTypeId,
+    'customer_status',
+    [
+        'type' => 'int',
+        'label' => 'Customer FreshSales ID',
+        'input' => 'text',
+        'forms' => ['adminhtml_customer'],
+        'required' => false,
+        'visible' => 1,
+        'position' => 110,
+        'default' => 'Inactive',
+    ]
+);
+
+$installer->addAttributeToGroup(
+    $entityTypeId,
+    $attributeSetId,
+    $attributeGroupId,
+    'customer_status',
+    100
+);
+
+$oAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'customer_status');
+$oAttribute->setData('used_in_forms', ['adminhtml_customer']);
+$oAttribute->save();
+
+$installer->endSetup();
