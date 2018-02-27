8d3cb9761ce96fc0b2ecfaec40a01204aae20455
chajr <chajr@bluetree.pl>
2018-02-27 17:45:13 +0100

    Added some classes placeholders

---
+++ app/code/local/Chajr/FreshSales/Block/Main.php
@@ -0,0 +1,6 @@
+<?php
+
+class Chajr_FreshSales_Block_Main extends Mage_Core_Block_Abstract
+{
+    
+}
---
+++ app/code/local/Chajr/FreshSales/Model/Main.php
@@ -0,0 +1,6 @@
+<?php
+
+class Chajr_FreshSales_Model_Main extends Mage_Core_Model_Abstract
+{
+    
+}
