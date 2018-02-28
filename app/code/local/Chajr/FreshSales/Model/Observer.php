7286e6e43eb9b1c645d1d890369a8f6c79d888b6
chajr <chajr@bluetree.pl>
2018-02-28 12:28:54 +0100

    Added for observer curl lib loading and prepare headers

--- app/code/local/Chajr/FreshSales/Model/Observer.php
+++ app/code/local/Chajr/FreshSales/Model/Observer.php
@@ -2,11 +2,35 @@
 
 class Chajr_FreshSales_Model_Observer extends Mage_Core_Model_Abstract
 {
+    /**
+     * @param Varien_Event_Observer $observer
+     */
     public function customerRegisterSuccess(Varien_Event_Observer $observer)
+    {
+        $this->loadCurlLib();
+        $curl = new Mage_HTTP_Client_Curl;
+
+        $apiKey = Mage::getStoreConfig('chajr_freshsales/chajr_freshsales/chajr_freshsales_input');
+
+        $curl->setHeaders([
+            'Content-Type:application/json',
+            'Accept:application/json',
+            'Authorization: Token token=' . $apiKey,
+        ]);
+    }
+
+    protected function loadCurlLib()
+    {
+        require_once Mage::getBaseDir('lib') . DS . 'Mage' . DS . 'HTTP' . DS . 'Client' . DS . 'Curl.php';
+    }
+
+    protected function getCustomerData(Varien_Event_Observer $observer)
     {
         $event = $observer->getEvent();
         $customer = $event->getCustomer();
-        $email = $customer->getEmail();
-        Mage::log('Successfully logged in: ' . implode(',', [$customer, $email]), null, 'mylogfile.log');
+
+        return [
+            
+        ];
     }
 }
