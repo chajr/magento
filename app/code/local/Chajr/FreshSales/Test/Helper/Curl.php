e8b70dc137680941d4b69da22ae9963f5f0ec4fd
chajr <chajr@bluetree.pl>
2018-03-04 16:17:50 +0100

    Added Curl helper test

---
+++ app/code/local/Chajr/FreshSales/Test/Helper/Curl.php
@@ -0,0 +1,44 @@
+<?php
+
+class Chajr_FreshSales_Test_Helper_Curl extends EcomDev_PHPUnit_Test_Case
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testCurlExecution()
+    {
+        /** @var PHPUnit_Framework_MockObject_MockObject|Chajr_FreshSales_Helper_Curl $mock */
+        $mock = $this->getMockBuilder('Chajr_FreshSales_Helper_Curl')
+            ->setMethods(['handleCurlResponse'])
+            ->getMock();
+
+        $mock->method('handleCurlResponse')
+            ->will($this->returnValue($this->sampleData()));
+
+        $this->assertEquals(
+            $this->sampleData(),
+            $mock->post('someUrl.com', 'some data')
+        );
+
+        $this->assertEquals(
+            $this->sampleData(),
+            $mock->makeRequest(
+                'someUrl.com',
+                'some data',
+                [],
+                ['post' => 1]
+            )
+        );
+    }
+
+    protected function sampleData()
+    {
+        return [
+            'code' => 200,
+            'error' => '',
+            'response' => '{"lead":{"id":3000344926}}',
+        ];
+    }
+}
